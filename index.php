0e1ef096e9068a0744090871e75d73154679bc67
Cade Guevara <cadeguevara@gmail.com>
2014-11-04 12:28:58 -0800

    no message

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+//inserts the coding that was previously above
+require_once(__DIR__. "/view/header.php");
+require_once (__DIR__ . "/view/navigation.php");
+require_once(__DIR__ . "/controller/create-db.php");
+require_once(__DIR__ . "/view/footer.php");
+?>
+
+
--- view/navigation.php
+++ view/navigation.php
@@ -1,8 +1,12 @@
+<?php
+require_once(__DIR__ . "/../model/config.php");
+?>
+
 <nav>
 	<ul>
-		<<!--points to our post.php file -->
+		<!--points to our post.php file -->
 		<li>
-		<a href="../post.php"></a>
+		<a href="<?php echo $path . "post.php" ?>">blog post form</a>
 		</li>
 	</ul>
 </nav>
