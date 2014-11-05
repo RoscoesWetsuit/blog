a571ffdb2b7e04c885953bc8c51d7853f4594cad
Cade Guevara <cadeguevara@gmail.com>
2014-11-05 13:42:10 -0800

    more css

--- view/header.php
+++ view/header.php
@@ -3,6 +3,7 @@
 <body background="CSS/mooning.png">
 <head>
 <link rel="stylesheet" type="text/css" href="CSS/main.css">
+<link rel="stylesheet" type="text/css" href="CSS/bootstrap.min.css">
 	<title></title>
 </head>
 <body>
