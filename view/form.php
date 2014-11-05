3ce398f0154c5402d5c93bb563b7591b731bf44c
Cade Guevara <cadeguevara@gmail.com>
2014-11-05 12:01:05 -0800

    created a blog post form and used css

---
+++ view/form.php
@@ -0,0 +1,14 @@
+<h1>create blog post</h1>
+	
+	<!--form takes in info and send it to the table  -->
+<form>
+	<div>
+		<label for="title">title: </label>
+		<input type"text" name="title" />
+	</div>
+		
+	<div>
+		<label for="post">post: </label>
+		<textarea name="post"></textarea>
+	</div>
+</form>
--- view/header.php
+++ view/header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
+<body background="CSS/mooning.png">
 <head>
+<link rel="stylesheet" type="text/css" href="CSS/main.css">
 	<title></title>
 </head>
 <body>
