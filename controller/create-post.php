d648fd9910b70e4481dec9439f160b3193d1ef68
Cade Guevara <cadeguevara@gmail.com>
2014-11-14 12:13:48 -0800

    finished video 24: Querying our Database to Submit a Blog Post

---
+++ controller/create-post.php
@@ -0,0 +1,22 @@
+<?php
+//lets us insert info from our database
+require_once(__DIR__ . "/../model/database.php");
+//creating a connection to the database
+$connection = new mysqli($host, $username, $password, $database);
+
+
+//filters through title to make sure there is no hacks
+$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+//same thing but through posts
+$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+//this query is to insert things into our table
+$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
+
+if($query) {
+	echo "<p>Successfully inserted post: $title</p>";
+}
+else {
+	echo "<p>$connectio->error</p>";
+}
+
+$connection->close();
