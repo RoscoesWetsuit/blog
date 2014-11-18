6d20ed2b3e5765992f61335b7771ec1b99e9165e
Cade Guevara <cadeguevara@gmail.com>
2014-11-18 11:39:18 -0800

    finished video 27: Creating a Constructor for our Database Class

--- model/database.php
+++ model/database.php
@@ -1,1 +1,9 @@
 <?php
+//we make an instance of a class to create an object
+class Database {
+	private $connection;
+	private $host;
+	private $username;
+	private $password;
+	private $Database;
+}
