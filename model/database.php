c2250ee67958f75efaf77e8823cdb3006ef6dada
Cade Guevara <cadeguevara@gmail.com>
2014-11-18 12:13:29 -0800

    finished video 28: Creating Functions within our Database Class

--- model/database.php
+++ model/database.php
@@ -6,4 +6,27 @@ class Database {
 	private $username;
 	private $password;
 	private $Database;
+	//its public because we need it to be accessed wherever in any file
+	//msqli needs a constuctor function, so now wee
+	public function __construct($host, $usernamer, $password, $database) {
+		$this->host= $host;
+		$this->username = $usernme;
+		$this->password = $password;
+		$this->database = $database;
+	}
+
+	//opening a connection to the database
+	public function openConnection() {
+
+	}
+
+	//closing a connection to the database
+	public function closeConection() {
+
+
+	}
+	//creates a query
+	public function query($string) {
+
+	}
 }
