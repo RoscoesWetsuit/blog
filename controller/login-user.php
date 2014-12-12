8939c3d80c896e8be500478f2e135bf3f4c2eca9
Cade Guevara <cadeguevara@gmail.com>
2014-12-12 12:10:52 -0800

    finished video 48: Checking the Passwords for Login

--- controller/login-user.php
+++ controller/login-user.php
@@ -5,3 +5,17 @@
 	$password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
 	$query = $_session["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
+
+	if($query-num_rows == 1) {
+		$row = $query->fetch_array();
+
+		if($row["password"] == crypt($password, $row["salt"])) {
+			echo "<p>Login was successful</p>";
+		}
+		else {
+			echo "<p>Invalid username and password";
+		}
+	}
+	else {
+		echo"<p>Invalid username and password</p>";
+	}
--- controller/read-posts.php
+++ controller/read-posts.php
@@ -8,7 +8,7 @@
 	if($result) {
 		while($row = mysqli_fetch_array($result)) {
 			echo "<div class='post'>";
-			echo "<h2>" . $row['title'] . "</h2>";
+			echo "<h2>" . $row['TITLE'] . "</h2>";
 			echo "<br />";
 			echo "<p>" . $row['post'] . "</h1>";
 			echo "<br/>";
