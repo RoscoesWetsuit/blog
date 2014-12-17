824a2bc2617f2fbdf5e64b4407182a9f36d3d0c8
Cade Guevara <cadeguevara@gmail.com>
2014-12-16 18:35:55 -0800

    stylin' it up

--- view/register-form.php
+++ view/register-form.php
@@ -4,28 +4,24 @@ require_once(__DIR__ . "/../model/config.php");
 ?>
 
 
-<h1>Register</h1>
+<h1 class="register">Register</h1>
 
 <form method="post" action="<?php echo $path . "controller/create-user.php"; ?>">
 <!-- giving the ability to sign up -->
-<div>
-	<label for="email">Email: </label>
-	<input type="text" name="email" />
+<div class="email">
+ <p><input type="text" class="form-control" placeholder="Email"></p>
+</div>
+
+<div class="username">
+<p><input type="text" class="form-control" placeholder="username"></p>
+</div>
+
+<div class="password">
+    <p><input type="password" class="form-control" placeholder="Password"></p>
 </div>
 
 <div>
-	<label for="username">Username: </label>
-	<input type="text" name="username" />
-</div>
-
-<div>
-	<label for="Password">Password: </label>
-	<input type="password" name="password" />
-
-</div>
-
-<div>
-	<button type="submit">Submit</button>
+	 <input type="submit" value="submit"/>
 </div>
 </form>
 
